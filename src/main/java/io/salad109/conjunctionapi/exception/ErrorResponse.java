55727da3aed3a6cac177007c9274c73742a9fff3
Salad <szolek1337@gmail.com>
2025-12-18 20:58:08 +0100

    Update exception handler to follow RFC 9457 spec

--- src/main/java/io/salad109/conjunctionapi/exception/ErrorResponse.java
+++
@@ -1,9 +0,0 @@
-package io.salad109.conjunctionapi.exception;
-
-public record ErrorResponse(
-        int status,
-        String error,
-        String details,
-        String timestamp
-) {
-}
--- src/main/java/io/salad109/conjunctionapi/exception/GlobalExceptionHandler.java
+++ src/main/java/io/salad109/conjunctionapi/exception/GlobalExceptionHandler.java
@@ -1,30 +1,31 @@
 package io.salad109.conjunctionapi.exception;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.Instant;
+import java.net.URI;
 
-@ControllerAdvice
-public class GlobalExceptionHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleUnknownException(Exception ex) {
+    ProblemDetail handleUnknownException(Exception ex, HttpServletRequest request) {
         log.error("An unexpected error occurred", ex);
-        String details = ex.getMessage() != null ? ex.getMessage() : "Internal Server Error";
-        ErrorResponse errorResponse = new ErrorResponse(
-                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
-                details,
-                Instant.now().toString()
-        );
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        problemDetail.setTitle("Internal Server Error");
+        problemDetail.setDetail("An unexpected error occurred");
+        problemDetail.setInstance(URI.create(request.getRequestURI()));
+
+        return problemDetail;
     }
 }
